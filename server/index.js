46c88530ab50416af92cf0a8ee0b9d46cf05e178
HZie <boss.hz734@gmail.com>
2022-09-26 17:16:58 +0900

    create index.js

---
+++ server/index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(express.json());
+app.get('/', (req, res) => res.send('linked!'));
+
+app.listen(8001, () => console.log('Server Up and runing at 8001'));

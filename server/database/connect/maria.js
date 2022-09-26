6bf232f979bde48d5030254e08c3a100fcadfb09
HZie <boss.hz734@gmail.com>
2022-09-26 18:42:18 +0900

    connect to maria

--- server/database/connect/maria.js
+++ server/database/connect/maria.js
@@ -2,7 +2,7 @@ const maria = require('mysql');
 
 const conn = maria.createConnection({
   host: '35.78.66.52',
-  port: 4245,
+  port: 3306,
   user: 'test',
   password: 'test1234',
   database: 'testdb',

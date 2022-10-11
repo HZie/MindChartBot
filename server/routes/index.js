a9187d8e6c779ed4fce1a9483f2623157a2ee6e4
HZie <boss.hz734@gmail.com>
2022-10-11 18:12:18 +0900

    mariadb show tables

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,8 +2,23 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+// require maria.js
+const maria = require('../database/maria');
+
+// show tables
+router.get('/tables', (req, res) => {
+  maria.query('SHOW TABLES', (err, rows, fields) => {
+    if (!err) {
+      res.send(rows);
+    } else {
+      console.log('err: ' + err);
+      res.send(err);
+    }
+  });
 });
 
 module.exports = router;
